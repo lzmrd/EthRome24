2510d50cb10894d4e58db8340336346fc7522812
lzmrd <vicari.fed@gmail.com>
2024-10-05 12:56:15 +0200

    merged

--- packages/nextjs/app/pimlico/clientSetup.ts
+++ packages/nextjs/app/pimlico/clientSetup.ts
@@ -1,17 +1,18 @@
-// clientSetup.ts
-import { createPublicClient, http } from 'viem';
-import { createPimlicoClient } from 'permissionless/clients/pimlico';
-import { sepolia } from 'viem/chains';
- 
-export const publicClient = createPublicClient({
-  transport: http('https://rpc.ankr.com/eth_sepolia'),
-});
+import { createPimlicoClient } from "permissionless/clients/pimlico";
+import { Chain, http } from "viem";
+import { entryPoint07Address } from "viem/account-abstraction";
 
-export const pimlicoClient = createPimlicoClient({
-  chain: sepolia,
-  transport: http('https://api.pimlico.io/v2/sepolia/rpc?apikey=pim_2oXcvbCpjVnEf7QP8KL8G8'),
-  entryPoint: {
-    address: '0x0576a174D229E3cFA37253523E645A78A0C91B57', // Sostituisci con l'address dell'entry point corretto
-    version: '0.6',
-  },
-});
+if (!process.env.NEXT_PUBLIC_PIMLICO_API_KEY)
+  throw new Error("Missing NEXT_PUBLIC_PIMLICO_API_KEY");
+
+export const pimlicoRpcUrl = (chain: Chain) =>
+  `https://api.pimlico.io/v2/${chain.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
+
+export const pimlicoClient = (chain: Chain) =>
+  createPimlicoClient({
+    transport: http(pimlicoRpcUrl(chain)),
+    entryPoint: {
+      address: entryPoint07Address,
+      version: "0.6",
+    },
+  });
